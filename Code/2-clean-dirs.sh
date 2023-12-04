66d14538167a6d3c2a22bd4ed167d5dbee62e491
Atesam Abdullah <atesamabdullah8@gmail.com>
2023-12-05 00:35:03 +0500

    added step wise code files

---
+++ Code/2-clean-dirs.sh
@@ -0,0 +1,5 @@
+# The directories for test contain some garbage files and leftover mac build files
+cd MELD.Raw/raw/test_raw
+
+rm -r final* #larger multi dialogue files
+rm ._* # files made by MacOs
