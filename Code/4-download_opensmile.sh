994ff193535651b1e20c5b3eebe835e32c6a0202
Atesam Abdullah <atesamabdullah8@gmail.com>
2023-12-06 01:01:40 +0500

    downloaded and setup opensmile

---
+++ Code/4-download_opensmile.sh
@@ -0,0 +1,8 @@
+# download opensmile pre-built for linux
+wget -c 'https://github.com/audeering/opensmile/releases/download/v3.0.2/opensmile-3.0.2-linux-x86_64.zip'
+# unzip file
+unzip opensmile-3.0.2-linux-x86_64.zip
+# move zip to tar folder
+mv opensmile-3.0.2-linux-x86_64.zip tar/
+# rename folder for ease of use
+mv opensmile-3.0.2-linux-x86_64 opensmile
